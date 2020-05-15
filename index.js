60bc67c0f4cb9dc60fa0daea16cceb78c53b74a9
Ankur Agarwal <agarg2311@gmail.com>
2020-05-15 21:28:36 +0530

    First commit after creating basic directories

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+// use for adding layout which installed by npm install express-ejs-layouts
+const expresslayouts = require('express-ejs-layouts');
+app.use(expresslayouts);
+
+//for static css
+app.use(express.static('./assets'));
+
+// 
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+//use express router
+app.use('/', require('./routes'));
+
+// set up the view engine after installing the ejs
+app.set('view engine', 'ejs');
+app.set('views','./views')
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+    }
+    
+    console.log(`Server is running on port: ${port}`);
+});

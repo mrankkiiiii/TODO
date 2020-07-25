0beb0404f4af14a1a5aef924ef6f59233dfd242d
Ankur Agarwal <agarg2311@gmail.com>
2020-07-25 16:10:54 +0530

    home route is set

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,4 @@
 const Event = require('../models/events');
 module.exports.home = function(req,res){
-    Event.find({},function(err,event){
-        if(err)
-        {
-            console.log('Error in fetching events from DB');
-            return;
-        }
-        return res.render('event',{
-            title: "Todo App",
-            event_List: event
-        });
-   });
+    return res.render('home',{title :'home'})
 }
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const userController = require('../controllers/user_controller');
 router.get('/sign-in', userController.signin);
 router.get('/sign-up', userController.signup);
 router.post('/create', userController.create);
-router.post('/create-session', userController.createSession);
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect:'/user/sign-in'}
+    ), userController.createSession);
 module.exports = router;
